45e2c402d05ce9e30750a10c51f86fa5790a4c0a
Ya Cheng <yacheng3249@gmail.com>
2023-06-19 01:52:28 +0800

    feat: serverless framework setting

--- server/handler.js
+++ server/handler.js
@@ -9,23 +9,36 @@ const express = require("express");
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: async ({ req }) => {
+  context: async ({ event, context, express }) => {
     const context = {
       secret: process.env.JWT_SECRET,
       saltRounds: Number(process.env.SALT_ROUNDS),
     };
-    const token = req.headers.authorization;
+    const token = event.headers.authorization;
     if (token && token.startsWith("Bearer ")) {
       try {
         // Check the token and retrieve the decoded data
         const user = await jwt.verify(token.slice(7), context.secret);
         // Put the data into the context
-        return { ...context, user };
+        return {
+          headers: event.headers,
+          functionName: context.functionName,
+          event,
+          context,
+          expressRequest: express.req,
+          user,
+        };
       } catch (e) {
         throw new Error("Your session expired. Sign in again.");
       }
     }
-    return context;
+    return {
+      headers: event.headers,
+      functionName: context.functionName,
+      event,
+      context,
+      expressRequest: express.req,
+    };
   },
   playground: {
     endpoint: "/dev/graphql",
